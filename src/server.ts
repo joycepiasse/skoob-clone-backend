c82c3e0c18872e83bcb99214f1c2fba38b1d7c1f
joycepiasse <joycepiasse@outlook.com>
2026-02-05 14:52:11 -0300

    feat(SKBC-6): criar endpoint de login

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import app from "./app";
 
 const PORT = 3000;
+
+console.log("JWT_SECRET carregado?", process.env.JWT_SECRET ? "SIM" : "NÃO");
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
